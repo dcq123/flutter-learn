169eb91df381129bde1ec6e891f22b3ae0766eae
丁长青 <qing_soft@sina.cn>
2019-11-12 14:46:00 +0800

    添加顶部菜单

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
       initialRoute: "home",
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -32,33 +32,57 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: "menu",
+          onPressed: () {
+            T.showToast("菜单");
+          },
+        ),
         title: Text(widget.title),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-              leading: Icon(Icons.local_airport),
-              title: Text("基础控件"),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                Navigator.pushNamed(context, "base/widget");
-              }),
-          Divider(height: 1),
-          ListTile(
-              leading: Icon(Icons.line_style),
-              title: Text("基础列表"),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                Navigator.pushNamed(context, "list/base");
-              }),
-          Divider(height: 1)
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              T.showToast("search");
+            },
+          )
         ],
       ),
+      body: HomeListView(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => T.showToast("我是浮动按钮"),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class HomeListView extends StatelessWidget {
+  const HomeListView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+            leading: Icon(Icons.local_airport),
+            title: Text("基础控件"),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, "base/widget");
+            }),
+        Divider(height: 1),
+        ListTile(
+            leading: Icon(Icons.line_style),
+            title: Text("基础列表"),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, "list/base");
+            }),
+        Divider(height: 1)
+      ],
     );
   }
 }
